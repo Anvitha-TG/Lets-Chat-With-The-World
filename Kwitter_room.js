a4025d350758a5a989b09d95c77411faf806c433
Anvitha-TG <86748123+Anvitha-TG@users.noreply.github.com>
2022-03-10 19:13:53 -0800

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAl1LxGRR3RuKIaOT5KmwzhqBrYi9aPMRE",
+    authDomain: "kwitter-practice-ac568.firebaseapp.com",
+    databaseURL: "https://kwitter-practice-ac568-default-rtdb.firebaseio.com",
+    projectId: "kwitter-practice-ac568",
+    storageBucket: "kwitter-practice-ac568.appspot.com",
+    messagingSenderId: "707955382273",
+    appId: "1:707955382273:web:08f6de69095ea3747dc800",
+    measurementId: "G-4QEC2C7K1G"
+  };
