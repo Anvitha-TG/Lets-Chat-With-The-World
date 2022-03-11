ea665cf61fc7e091eea9d7ae36fe9ba3f6b99f9f
Anvitha-TG <86748123+Anvitha-TG@users.noreply.github.com>
2022-03-10 19:21:54 -0800

    Add files via upload

--- Kwitter_room.js
+++ Kwitter_room.js
@@ -8,3 +8,32 @@ const firebaseConfig = {
     appId: "1:707955382273:web:08f6de69095ea3747dc800",
     measurementId: "G-4QEC2C7K1G"
   };
+
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+    Room_names = childKey;
+   //Start code
+   console.log("Room Name - " + Room_names);
+   row = "<div class='room_name' id="+Room_names+"onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+   document.getElementById("output").innerHTML += row;
+   //End code
+   });});}
+getData();
+
+function addRoom() {
+   room_name = document.getElementById("room_name").value;
+
+   firebase.database().ref("/").child(room_name).uptate({
+         purpose : "adding room name"
+   });
+
+     localStorage.setItem("room_name", room_name);
+
+     window.location = "kwitter_page.html";
+}
+
+
+function redirectToRoomName(name){
+   console.log(name);
+   localStorage.setItem("room_name", name);
+   window.location= "kwitter_page.html"
+}
